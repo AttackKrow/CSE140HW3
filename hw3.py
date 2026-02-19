ff86abdfb6e23a7d366d9e64250af67e4a6b44cd
AttackKrow <forcenicholas@gmail.com>
2026-02-19 13:43:50 -0800

    init

---
+++ hw3.py
@@ -0,0 +1,4 @@
+#Take in user input with length checks
+#Get opcode and use to determine instruction type
+#Grab and fill relevant fields based on instruction type
+#Print instruction fields in the expected format
