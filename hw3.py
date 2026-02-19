d299b4ee99bb4a31b73c119d58d0bf36eec74b7d
Krish Sarkar <krishsarkar@ucmerced.edu>
2026-02-19 14:10:04 -0800

    Get opcode and use to determin instruction type

--- hw3.py
+++ hw3.py
@@ -1,4 +1,13 @@
 #Take in user input with length checks
+
 #Get opcode and use to determine instruction type
+  opcode   = inst & 0x7F
+  rd       = (inst >> 7) & 0x1F
+  funct3   = (inst >> 12) & 0x7
+  rs1      = (inst >> 15) & 0x1F 
+  rs2      = (inst >> 20) & 0x1F
+  funct7 =   (inst >> 25) & 0x7F
+
 #Grab and fill relevant fields based on instruction type
+
 #Print instruction fields in the expected format
